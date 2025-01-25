3e98847dccb2ccb758dad554ed000d52f1df829a
k03302 <unisonman660@gmail.com>
2025-01-25 14:46:09 +0900

    Add utils

---
+++ include/utils.h
@@ -0,0 +1,17 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+inline int clamp(int value, int min, int max)
+{
+    if (value > max)
+    {
+        return max;
+    }
+    if (value < min)
+    {
+        return min;
+    }
+    return value;
+}
+
+#endif
