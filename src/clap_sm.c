5cccc5c18e026239b3de230cccb5abb8a8cc5982
k03302 <unisonman660@gmail.com>
2025-01-25 21:14:34 +0900

    Refactor clap state machine with state pattern

--- src/clap_sm.c
+++ src/clap_sm.c
@@ -4,96 +4,149 @@
 #include "adc_ctrl.h"
 #include "led.h"
 
+/*
+    다음과 같은 패턴의 2회의 박수를 인식한다.
+    CLAP_SECOND_BOTTOM 상태가 되면 finished 상태가 된다.
+    박수를 인식하는 조건은 다음과 같다.
+
+         /\            /\
+        /  \          /  \
+       /    \        /    \
+------/      \------/      \------
+
+    1. 박수소리가 너무 오래 지속되면 일반 소음으로 간주하여 초기화한다.
+    2. 박수소리가 너무 짧게 지속되면 지속시간이 확보될 때까지 무시한다.
+    3. 두 번째 박수가 너무 늦게 시작하면 첫 번째 박수로 간주한다.
+*/
+
+// 박수의 시작과 끝 타임스탬프를 저장
+static long long start_timestamp = 0;
+static long long end_timestamp = 0;
+
+// 박수 상태
 enum ClapState
 {
-    CLAP_START,
-    CLAP_FIRST_RISE,
-    CLAP_FIRST_DROP,
-    CLAP_SECOND_RISE,
-    CLAP_SECOND_DROP
+    CLAP_START,         // 대기 상태
+    CLAP_FIRST_TOP,     // 첫 번째로 소리가 역치 초과
+    CLAP_FIRST_BOTTOM,  // 첫 번째로 역치 미만으로 하강
+    CLAP_SECOND_TOP,    // 두 번째로 소리가 역치 초과
+    CLAP_SECOND_BOTTOM, // 두 번째로 역치 미만으로 하강
+    CLAP_STATE_COUNT    // 상태 개수
 };
 
-/*
-박수 상태머신 관련 변수
-*/
-static enum ClapState clap_state = CLAP_START;
-static long long clap_start_timestamp = 0;
-static long long clap_end_timestamp = 0;
+static enum ClapState current_state = CLAP_START;
 
-int clap_state_machine_finished()
+typedef void (*ClapStateFunc)(void);
+
+// 상태 함수
+static void state_start(void);
+static void state_top_common(void);
+static void state_first_bottom(void);
+static void state_second_bottom(void);
+
+static ClapStateFunc state_table[CLAP_STATE_COUNT] = {
+    state_start,
+    state_top_common,
+    state_first_bottom,
+    state_top_common,
+    state_second_bottom};
+
+int clap_state_machine_finished(void)
 {
-    return clap_state == CLAP_SECOND_DROP;
+    return current_state == CLAP_SECOND_BOTTOM;
 }
 
-void clap_state_machine_initialize()
+void clap_state_machine_initialize(void)
 {
-    clap_state = CLAP_START;
-
-    clap_start_timestamp = 0;
-    clap_end_timestamp = 0;
+    current_state = CLAP_START;
+    start_timestamp = 0;
+    end_timestamp = 0;
 }
 
-void clap_state_machine()
+void clap_state_machine(void)
 {
     assert(adc_get_current_channel() == ADC_CHANNEL_SOUND);
+    state_table[current_state]();
+}
 
-    int sound_value_realtime = adc_read(ADC_CHANNEL_SOUND);
-    int sound_threshold = system_get_attribute(SOUND_THRESHOLD);
+// STATE_START 상태 함수
+static void state_start(void)
+{
+    assert(current_state == CLAP_START);
+    int sound_value = adc_read(ADC_CHANNEL_SOUND);
+    int threshold = system_get_attribute(SOUND_THRESHOLD);
 
-    switch (clap_state)
+    // 소리가 역치 초과
+    if (sound_value > threshold)
     {
-
-    case CLAP_START:
-        if (sound_value_realtime > sound_threshold)
-        {
-            clap_start_timestamp = timer_get_time();
-            clap_state = CLAP_FIRST_RISE;
-        }
-        break;
-
-    case CLAP_FIRST_RISE:
-    case CLAP_SECOND_RISE:
-        if (sound_value_realtime < sound_threshold)
-        {
-            clap_end_timestamp = timer_get_time();
-            int clap_duration = clap_end_timestamp - clap_start_timestamp;
-
-            if (clap_duration < system_get_attribute(CLAP_MIN_DURATION))
-            {
-            }
-            else if (clap_duration > system_get_attribute(CLAP_MAX_DURATION))
-            {
-                clap_state = CLAP_START;
-            }
-            else
-            {
-                clap_state = (clap_state == CLAP_FIRST_RISE) ? CLAP_FIRST_DROP : CLAP_SECOND_DROP;
-            }
-        }
-        break;
-
-    case CLAP_FIRST_DROP:
-        if (sound_value_realtime > sound_threshold)
-        {
-            clap_start_timestamp = timer_get_time();
-            int clap_gap = clap_start_timestamp - clap_end_timestamp;
-
-            if (clap_gap < system_get_attribute(CLAP_MIN_GAP))
-            {
-            }
-            else if (clap_gap > system_get_attribute(CLAP_MAX_GAP))
-            {
-                clap_state = CLAP_FIRST_RISE;
-            }
-            else
-            {
-                clap_state = CLAP_SECOND_RISE;
-            }
-        }
-        break;
-
-    case CLAP_SECOND_DROP:
-        led_clear();
-        break;
+        start_timestamp = timer_get_time();
+        current_state = CLAP_FIRST_TOP;
     }
+}
+
+// STATE_FIRST_TOP, STATE_SECOND_TOP 상태 함수
+static void state_top_common(void)
+{
+    assert(current_state == CLAP_FIRST_TOP || current_state == CLAP_SECOND_TOP);
+    int sound_value = adc_read(ADC_CHANNEL_SOUND);
+    int threshold = system_get_attribute(SOUND_THRESHOLD);
+
+    // 소리가 역치 미만으로 하강
+    if (sound_value < threshold)
+    {
+        end_timestamp = timer_get_time();
+        int duration = end_timestamp - start_timestamp;
+
+        if (duration < system_get_attribute(CLAP_MIN_DURATION))
+        {
+            // 박수 스파이크가 너무 짧게 지속됨. 무시
+        }
+        else if (duration > system_get_attribute(CLAP_MAX_DURATION))
+        {
+            // 박수 스파이크가 너무 길게 지속됨. 박수 아님 판정. 시작 상태로 복귀
+            current_state = CLAP_START;
+        }
+        else
+        {
+            // 정상적인 박수 스파이크 인식
+            current_state = (current_state == CLAP_FIRST_TOP) ? CLAP_FIRST_BOTTOM : CLAP_SECOND_BOTTOM;
+        }
+    }
+}
+
+// STATE_FIRST_BOTTOM 상태 함수
+static void state_first_bottom(void)
+{
+    assert(current_state == CLAP_FIRST_BOTTOM);
+    int sound_value = adc_read(ADC_CHANNEL_SOUND);
+    int threshold = system_get_attribute(SOUND_THRESHOLD);
+
+    // 소리가 역치 초과
+    if (sound_value > threshold)
+    {
+        start_timestamp = timer_get_time();
+        int gap = start_timestamp - end_timestamp;
+
+        if (gap < system_get_attribute(CLAP_MIN_GAP))
+        {
+            // 너무 짧은 박수 간 간격. 무시
+        }
+        else if (gap > system_get_attribute(CLAP_MAX_GAP))
+        {
+            // 너무 긴 박수 간 간격. 첫 번째 박수 스파이크로 간주
+            current_state = CLAP_FIRST_TOP;
+        }
+        else
+        {
+            // 정상적인 첫 번째 박수 간 간격 인식
+            current_state = CLAP_SECOND_TOP;
+        }
+    }
+}
+
+// STATE_SECOND_BOTTOM 상태 함수
+static void state_second_bottom(void)
+{
+    assert(current_state == CLAP_SECOND_BOTTOM);
+    led_clear();
 }
