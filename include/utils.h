9cff1c8078632dea4b3bcf0b1abb445754740445
k03302 <unisonman660@gmail.com>
2025-01-26 01:59:39 +0900

    Add min, max to utils

--- include/utils.h
+++ include/utils.h
@@ -16,4 +16,14 @@ inline int clamp(int value, int min, int max)
     return value;
 }
 
+inline int min(int a, int b)
+{
+    return a < b ? a : b;
+}
+
+inline int max(int a, int b)
+{
+    return a > b ? a : b;
+}
+
 #endif
