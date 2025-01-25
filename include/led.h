a04da2c065e0980adcc02d0c75c26d0ef9190eaf
k03302 <unisonman660@gmail.com>
2025-01-25 12:26:14 +0900

    Add led

---
+++ include/led.h
@@ -0,0 +1,27 @@
+#ifndef LED_H
+#define LED_H
+
+/*
+    @brief
+    LED 초기화 함수
+*/
+void led_init(void);
+
+/*
+    @brief
+    RGB LED 를 모두 끔
+*/
+void led_clear(void);
+
+/*
+    @brief
+    value 값을 start ~ end 범위에 따라 8개의 LED 에 표시
+
+    @param
+    value: 표시할 값 (start ~  end)
+    start: 최소값
+    end: 최대값
+*/
+void led_accumulate_print(int value, int start, int end);
+
+#endif
---
+++ src/led.c
@@ -0,0 +1,41 @@
+#include "common.h"
+#include "led.h"
+
+void led_init(void)
+{
+    LED_DDR = LED_ALL_BITS;
+    led_clear();
+}
+
+void led_clear(void)
+{
+    LED_BASE = 0x00;
+}
+
+void led_accumulate_print(int value, int start, int end)
+{
+    static_assert(start < end);
+
+    if (value >= start)
+    {
+        LED_BASE = 0xff;
+        return;
+    }
+
+    if (value <= end)
+    {
+        LED_BASE = 0x00;
+        return;
+    }
+
+    int led = 0x00;
+
+    int interval = (end - start) >> 3;
+    value -= start;
+    while (value > 0)
+    {
+        value -= interval;
+        led = (led << 1) | 0x01;
+    }
+    LED_BASE = led;
+}
