ea3b9a5305ca03c88c06710a50d157a80b989dec
k03302 <unisonman660@gmail.com>
2025-02-03 23:29:10 +0900

    Update lamp state machine

--- src/state_machines/lamp_sm.c
+++ src/state_machines/lamp_sm.c
@@ -11,45 +11,37 @@
 // 램프 상태
 enum LampState
 {
-    LAMP_DAY,              // 낮으로 간주되는 상태
-    LAMP_NIGHT,            // 밤으로 간주되는 상태
+    LAMP_CHECK_LIGHT,      // 조도를 인식하는 상태
     LAMP_CHECK_SOUND,      // 박수 소리를 인식하는 상태
     LAMP_ADC_CHANGE_SOUND, // ADC 채널 변경 후 안정화 대기 상태
     LAMP_ADC_CHANGE_LIGHT, // ADC 채널 변경 후 안정화 대기 상태
-    LAMP_WAIT,             // 박수 소리를 인식한 후 대기 상태
     LAMP_STATE_COUNT
 };
 
-static enum LampState lamp_mode = LAMP_DAY;
+static enum LampState lamp_mode = LAMP_CHECK_LIGHT;
 static struct watch adc_change_watch;
-static struct watch cds_check_watch;
-static struct watch clap_toggle_watch;
+static struct watch sound_check_watch;
 static int initialize_done = 0; // 초기화 함수를 호출했는지 여부
 
 // 상태 함수
-static void state_day(void);
-static void state_night(void);
+static void state_check_light(void);
+static void state_check_sound(void);
 static void state_adc_change_sound(void);
 static void state_adc_change_light(void);
-static void state_wait(void);
-static void state_check_sound(void);
 
 static StateFuncNoParam state_table[LAMP_STATE_COUNT] = {
-    state_day,
-    state_night,
+    state_check_light,
     state_check_sound,
     state_adc_change_sound,
-    state_adc_change_light,
-    state_wait};
+    state_adc_change_light};
 
 void lamp_state_machine_initialize()
 {
     initialize_done = 1;
-    lamp_mode = LAMP_DAY;
+    lamp_mode = LAMP_CHECK_LIGHT;
 
     watch_init(&adc_change_watch, system_get_attribute(SA_ADC_CHANGE_TIMEOUT));
-    watch_init(&cds_check_watch, system_get_attribute(SA_CDS_CHECK_PERIOD));
-    watch_init(&clap_toggle_watch, system_get_attribute(SA_CLAP_TOGGLE_WAIT));
+    watch_init(&sound_check_watch, system_get_attribute(SA_CDS_CHECK_PERIOD));
 
     adc_init(ADC_CHANNEL_CDS);
     led_init();
@@ -65,51 +57,15 @@ void lamp_state_machine()
 
 /*
     @brief
-    LAMP_DAY 상태 함수
-    조도를 확인하여 역치보다 낮으면 LAMP_NIGHT 상태로 전환
-*/
-static void state_day(void)
-{
-    int light_value_realtime = adc_read(ADC_CHANNEL_CDS);
-    if (light_value_realtime < system_get_attribute(SA_CDS_NIGHT_THRESHOLD))
-    {
-        rgb_led_set(1);
-        lamp_mode = LAMP_NIGHT;
-    }
-}
-
-/*
-    @brief
-    LAMP_NIGHT 상태 함수
-    조도를 확인하여 역치보다 높으면 LAMP_DAY 상태로 전환
-    역치보다 낮으면 LAMP_ADC_CHANGE_SOUND 상태로 전환
-*/
-static void state_night(void)
-{
-    int light_value_realtime = adc_read(ADC_CHANNEL_CDS);
-    if (light_value_realtime > system_get_attribute(SA_CDS_DAY_THRESHOLD))
-    {
-        rgb_led_set(0);
-        lamp_mode = LAMP_DAY;
-    }
-    else
-    {
-        watch_update(&cds_check_watch);
-        adc_init(ADC_CHANNEL_SOUND);
-        lamp_mode = LAMP_ADC_CHANGE_SOUND;
-    }
-}
-
-/*
-    @brief
     LAMP_ADC_CHANGE_SOUND 상태 함수
     잠시 대기 후 LAMP_CHECK_SOUND 상태로 전환
     ADC 채널 변경 후 안정화 시간 확보
 */
 static void state_adc_change_sound(void)
 {
-    if (watch_check_update(&adc_change_watch))
+    if (watch_check(&adc_change_watch))
     {
+        watch_update(&sound_check_watch);
         lamp_mode = LAMP_CHECK_SOUND;
     }
 }
@@ -117,51 +73,63 @@ static void state_adc_change_sound(void)
 /*
     @brief
     LAMP_ADC_CHANGE_LIGHT 상태 함수
-    잠시 대기 후 LAMP_NIGHT 상태로 전환
+    잠시 대기 후 LAMP_CHECK_LIGHT 상태로 전환
     ADC 채널 변경 후 안정화 시간 확보
 */
 static void state_adc_change_light(void)
 {
-    if (watch_check_update(&adc_change_watch))
+    if (watch_check(&adc_change_watch))
     {
-        lamp_mode = LAMP_NIGHT;
-    }
-}
-
-/*
-    @brief
-    LAMP_WAIT 상태 함수
-    잠시 대기 후 LAMP_CHECK_SOUND 상태로 전환
-    박수소리 2회 인식 후 일정 시간 동안 대기하기 위함
-*/
-static void state_wait(void)
-{
-    if (watch_check(&clap_toggle_watch))
-    {
-        lamp_mode = LAMP_CHECK_SOUND;
+        lamp_mode = LAMP_CHECK_LIGHT;
     }
 }
 
 /*
     @brief
     LAMP_CHECK_SOUND 상태 함수
-    박수 소리를 인식하여 LAMP_WAIT 상태로 전환
+    사운드센서 값을 읽어서 clap_state_machine에 전달
+    박수 인식 시 rgb led를 토글 및 빛 감지 시작
+    또는 일정 시간 경과 시 빛 감지 시작
 */
 static void state_check_sound(void)
 {
-    clap_state_machine();
-    if (watch_check(&cds_check_watch))
+    int sound_value_realtime = adc_read(ADC_CHANNEL_SOUND);
+    clap_state_machine(sound_value_realtime);
+
+    if (clap_state_machine_finished())
+    {
+        clap_state_machine_initialize();
+        rgb_led_toggle();
+
+        watch_update(&adc_change_watch);
+        adc_init(ADC_CHANNEL_CDS);
+        lamp_mode = LAMP_ADC_CHANGE_LIGHT;
+    }
+    else if (watch_check(&sound_check_watch))
     {
         watch_update(&adc_change_watch);
         adc_init(ADC_CHANNEL_CDS);
         lamp_mode = LAMP_ADC_CHANGE_LIGHT;
     }
-    else if (clap_state_machine_finished())
-    {
-        clap_state_machine_initialize();
-        rgb_led_toggle();
+}
 
-        lamp_mode = LAMP_WAIT;
-        watch_update(&clap_toggle_watch);
+/*
+    @brief
+    LAMP_CHECK_LIGHT 상태 함수
+*/
+static void state_check_light(void)
+{
+    int light_value_realtime = adc_read(ADC_CHANNEL_CDS);
+    if (light_value_realtime > system_get_attribute(SA_CDS_DAY_THRESHOLD))
+    {
+        rgb_led_set(1);
     }
+    else
+    {
+        rgb_led_set(0);
+    }
+
+    adc_init(ADC_CHANNEL_SOUND);
+    watch_update(&adc_change_watch);
+    lamp_mode = LAMP_ADC_CHANGE_SOUND;
 }
